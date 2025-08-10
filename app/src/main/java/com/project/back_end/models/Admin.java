831faf924c54d24f0530bc620063d366dd28f8f9
SameepSkillup <sameep.skilluponline@gmail.com>
2025-08-10 15:40:08 -0400

    Added Admin entity with validation and JSON security

---
+++ app/src/main/java/com/project/back_end/models/Admin.java
@@ -0,0 +1,55 @@
+package com.project.back_end.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "Username cannot be null")
+    private String username;
+
+    @NotNull(message = "Password cannot be null")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    // Default constructor
+    public Admin() {
+    }
+
+    // Parameterized constructor
+    public Admin(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getters & Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
